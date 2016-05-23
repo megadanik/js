c000f2cdb8e76de3d237262da9716fa164ced011
DenisSchetinkin <schetinkin.denis@gmail.com>
2016-05-23 20:21:53 +0300

    Initial commit

---
+++ 1.js
@@ -0,0 +1,6 @@
+name=prompt("Item call:");
+pricePerItem= +prompt("get price of item");
+count= +prompt("Get count of items:");
+alert("Thank you for buying" + name + "(s)."+ "Total:" + pricePerItem*count)/**
+ * Created by Brainacad2 on 23.05.2016.
+ */
